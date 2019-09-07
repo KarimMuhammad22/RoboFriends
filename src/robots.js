4e067211536fcee46f0cbf77591b8dc8a22a0bd8
karim muhammad <benzema473@yahoo.com>
2019-09-07 23:28:27 +0200

    adding robots cards

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Karim Muh",
+    username: "kemo",
+    email: "Karim.Muh@gmail.com"
+  },
+  {
+    id: 2,
+    name: "Mo Salah",
+    username: "kemo",
+    email: "moSalah@gmail.com"
+  },
+  {
+    id: 3,
+    name: "Mo Abotrika",
+    username: "kemo",
+    email: "Abotrika@gmail.com"
+  },
+  {
+    id: 4,
+    name: "Cristiano Ronaldo",
+    username: "kemo",
+    email: "Ronaldo@gmail.com"
+  },
+  {
+    id: 5,
+    name: "Emad Meteb",
+    username: "kemo",
+    email: "Meteb@gmail.com"
+  },
+  {
+    id: 6,
+    name: "Trezeget",
+    username: "kemo",
+    email: "Trezeget@gmail.com"
+  },
+  {
+    id: 7,
+    name: "Lionel Messi",
+    username: "kemo",
+    email: "Messi@gmail.com"
+  },
+  {
+    id: 8,
+    name: "Benzema",
+    username: "kemo",
+    email: "Benzema@gmail.com"
+  },
+  {
+    id: 9,
+    name: "Mo Zidan",
+    username: "kemo",
+    email: "Zidan@gmail.com"
+  },
+  {
+    id: 10,
+    name: "Amr Zaki",
+    username: "kemo",
+    email: "Zaki@gmail.com"
+  }
+];
