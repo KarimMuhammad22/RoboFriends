9915ad8a02c6ab8e675e0c6a138eb47c3ba6d7ac
karim muhammad <benzema473@yahoo.com>
2019-09-08 01:34:49 +0200

    Adding Search Box

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import CardList from "./CardList";
+import App from "./App";
 import "tachyons";
 import * as serviceWorker from "./serviceWorker";
-import { robots } from "./robots";
 
-ReactDOM.render(
-  <div>
-    <CardList robots={robots} />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
